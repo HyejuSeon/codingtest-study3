f6e8f3b779de41427dc7799331b26f366764bb88
HyejuSeon <hyejooseon@gmail.com>
2022-08-14 02:25:01 +0900

    트리의 지름

--- Hyeju/boj1967.py
+++ Hyeju/boj1967.py
@@ -1,29 +1,28 @@
 # 트리의 지름
-from sys import stdin
-from collections import deque
+import sys
+sys.setrecursionlimit(10**9)
 
-N = int(stdin.readline())
+N = int(sys.stdin.readline())
 A = [[] for _ in range(N + 1)]
 for _ in range(N - 1):
-    a, b, c = map(int, stdin.readline().split())
+    a, b, c = map(int, sys.stdin.readline().split())
     A[a].append((b, c))
     A[b].append((a, c))
 
-def bfs(s):
-    q = deque()
-    q.append(s)
-    dist[s] = 0
-    while q:
-        curr = q.popleft()
-        for e, w in A[curr]:
-            if dist[e] == -1:
-                dist[e] = dist[curr] + w
-                q.append(e)
+def dfs(n, d):
+    global res, max_idx
+    visited[n] = 1
+    if res < d:
+        res = d
+        max_idx = n
+    for next, next_dist in A[n]:
+        if not visited[next]:
+            dfs(next, next_dist + d)
 
-dist = [-1] * (N + 1)
-bfs(1)
-max_dist = max(dist)
-max_idx = dist.index(max_dist)
-dist = [-1] * (N + 1)
-bfs(max_idx)
-print(max(dist))
+res = 0
+max_idx = 0
+visited = [0] * (N + 1)
+dfs(1, 0)
+visited = [0] * (N + 1)
+dfs(max_idx, 0)
+print(res)
