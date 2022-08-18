7c089178deaf46b35ba25bc59775236d6befbf7d
eunhyekim1223 <eunhyekim1223@gmail.com>
2022-08-18 21:20:15 +0900

    WEEK 02 fixed

--- Eunhye/WEEK02/BOJ_1967_트리의 지름/boj_1967.py
+++ Eunhye/WEEK02/BOJ_1967_트리의 지름/boj_1967.py
@@ -1,10 +1,14 @@
-V = int(input())
-tree = [[] for _ in range(V+1)]
+import sys
+sys.setrecursionlimit(10**7)
+
+n = int(input())
+tree = [[] for _ in range(n+1)]
 result, max_node = 0, 0
-for _ in range(V):
-  input_lst = list(map(int, input().split()))
-  for i in range(1, len(input_lst)-1, 2):
-    tree[input_lst[0]].append(tuple(input_lst[i:i+2]))
+for _ in range(n-1):
+  p, ch, w = map(int, input().split())
+  tree[p].append((ch, w))
+  tree[ch].append((p, w))
+  
 
 def dfs(src, dist, visited):
   global result, max_node
@@ -12,11 +16,11 @@ def dfs(src, dist, visited):
   if dist > result:
     result = dist
     max_node = src
-  for v, d in tree[src]:
-    if not visited[v]:
-      dfs(v, dist+d, visited)
+  for ch, w in tree[src]:
+    if not visited[ch]:
+      dfs(ch, dist+w, visited)
 
-visited = [0 for _ in range(V+1)]
+visited = [0 for _ in range(n+1)]
 
 dfs(1, 0, visited[:])
 dfs(max_node, 0, visited[:])
